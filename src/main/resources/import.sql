e7003c58eb7eaef3b7fec66bb493552647a2fb5f
ProgIsLove <58989540+ProgIsLove@users.noreply.github.com>
2021-03-13 00:27:30 +0100

    Quarkus rest

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO person(id, firstName, lastName) VALUES (nextval('hibernate_sequence'), 'Jaromir','Jagr');
+INSERT INTO person(id, firstName, lastName) VALUES (nextval('hibernate_sequence'), 'Pavel','Datsyuk');
+INSERT INTO person(id, firstName, lastName) VALUES (nextval('hibernate_sequence'), 'Connor','McDavid');
+INSERT INTO person(id, firstName, lastName) VALUES (nextval('hibernate_sequence'), 'Sidney','Crosby');
